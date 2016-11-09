9dc34d097e9563141cd5088b6c39b8a4bec3756d
Ales Waiser <aleswaiser@gmail.com>
2016-11-09 15:11:20 +0300

    Конвертер полностью переписан

--- converter.php
+++ converter.php
@@ -1,55 +1,58 @@
 ﻿<?php
 define('SOURSE_PATH', '');
 
-function converter($value, $from_multiple = 'RUB', $to_multiple = 'RUB', $sourse = '') {
-	if (!get_from_cache($from_multiple, $to_multiple)) {
+function converter($value, $from_currency = 'RUB', $to_currency = 'RUB', $sourse = '') {
+	$from_currency_multiple = 1;
+	$to_currency_multiple = 1;
+	if (!get_from_cache($from_currency, $to_currency)) {
 		if (file_exists(SOURSE_PATH.'/'.$sourse.'.php')) {
 			include SOURSE_PATH.'/'.$sourse.'.php';
 		} else {
 			$context = stream_context_create(array(
-				'http'=> array(
+				'http' => array(
 					'timeout' => 1
 				)
 			));
 
-			$path = file_get_contents('http://www.cbr-xml-daily.ru/daily.xml', false, $context); // http://www.cbr.ru/scripts/XML_daily.asp
+			$url = 'http://www.cbr-xml-daily.ru/daily.xml'; // http://www.cbr.ru/scripts/XML_daily.asp
+
+			$path = file_get_contents($url, false, $context);
 
 			$xml = new SimpleXMLElement($path);
 
-			if ($from_multiple !== 'RUB') {
-				$from_multiple = $xml->xpath("//Valute/CharCode[text()='$from_multiple']/following-sibling::Value");
-				$from_multiple = $from_multiple[0];
+			if ($from_currency !== 'RUB') {
+				$from_currency_multiple = $xml->xpath("//Valute/CharCode[text()='$from_currency']/following-sibling::Value");
+				$from_currency_multiple = $from_currency_multiple[0];
 			}
 
-			if ($to_multiple !== 'RUB') {
-				$to_multiple = $xml->xpath("//Valute/CharCode[text()='$to_multiple']/following-sibling::Value");
-				$to_multiple = $to_multiple[0];
+			if ($to_currency !== 'RUB') {
+				$to_currency_multiple = $xml->xpath("//Valute/CharCode[text()='$to_currency']/following-sibling::Value");
+				$to_currency_multiple = $to_currency_multiple[0];
 			}
 		}
 
-		set_to_cache($from_multiple, $to_multiple);
+		set_to_cache($from_currency, $to_currency, $from_currency_multiple, $to_currency_multiple);
 	}
 
-	$final_multiple = get_from_cache($from_multiple, $to_multiple);
+	$final_multiple = get_from_cache($from_currency, $to_currency);
 
 	return $value * $final_multiple;
 }
 
-function get_from_cache($from_multiple, $to_multiple) {
+function get_from_cache($from_currency, $to_currency) {
 	$memcache_obj = memcache_connect('localhost', 11211);
 
-	$key = md5($from_multiple.$to_multiple);
+	$key = md5($from_currency.' '.$to_currency);
 
 	return memcache_get($memcache_obj, $key);
 }
 
-function set_to_cache($from_multiple, $to_multiple) {
+function set_to_cache($from_currency, $to_currency, $from_currency_multiple, $to_currency_multiple) {
 	$memcache_obj = memcache_connect('localhost', 11211);
 
-	$key = md5($from_multiple.$to_multiple);
+	$key = md5($from_currency.' '.$to_currency);
 
-	$final_multiple = $from_multiple / $to_multiple;
-
+	$final_multiple = $from_currency_multiple / $to_currency_multiple;
 	memcache_set($memcache_obj, $key, $final_multiple, 0, 43200);
 }
 ?>
